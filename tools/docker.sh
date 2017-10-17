c9c0d2e93ab7ac266139797f9ebd10b460268bc8
Yang Bin <yangkghjh@gmail.com>
2017-10-17 12:16:12 -0400

    add test files

--- tools/docker.sh
+++ tools/docker.sh
@@ -11,13 +11,47 @@ cd ..
 
 list=( \
     "analysis-nodejs-eslint" \
-    )
+    "analysis-nodejs-flow" \
+    "analysis-nodejs-jscs" \
+    "analysis-nodejs-jshint" \
+    "build-nodejs-babel" \
+    "build-nodejs-browserify" \
+    "build-nodejs-brunch" \
+    "build-nodejs-coffee" \
+    "build-nodejs-grunt" \
+    "build-nodejs-gulp" \
+    "build-nodejs-less" \
+    "build-nodejs-uglify" \
+    "build-nodejs-webpack" \
+    "dependence-nodejs-bower" \
+    "dependence-nodejs-npm" \
+    "dependence-nodejs-yarn" \
+    "document-nodejs-docco" \
+    "document-nodejs-jsdoc" \
+)
 
 paths=( \
     "cd docker/Analysis/eslint" \
+    "cd ../flow" \
+    "cd ../jscs" \
+    "cd ../jshint" \
+    "cd ../../Build/babel" \
+    "cd ../browserify" \
+    "cd ../brunch" \
+    "cd ../coffee" \
+    "cd ../grunt" \
+    "cd ../gulp" \
+    "cd ../less" \
+    "cd ../uglify" \
+    "cd ../webpack" \
+    "cd ../../Dependence/bower" \
+    "cd ../npm" \
+    "cd ../yarn" \
+    "cd ../../Document/docco" \
+    "cd ../jsdoc" \
 )
 
-for i in {0..0}; do
+for i in {0..17}; do
     ${paths[$i]}
     echo "Build $PREFIX${list[$i]}:$VERSION"
     docker build -t $PREFIX${list[$i]}:$VERSION .
